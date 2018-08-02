42899920d0f54efd70c3623401f96ff9baec1c43
blueshinymarble <blueshinymarble@gmail.com>
2018-08-02 21:54:44 +0900

    project began

---
+++ Assets/Scripts/RockInstantiator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockInstantiator : MonoBehaviour
+{
+    public int maxStageRocks;
+    public GameObject[] rocks;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SpawnRocksOnStart()
+    {
+        for (int i = 0; i < maxStageRocks; i++)
+        {
+
+        }
+    }
+}
